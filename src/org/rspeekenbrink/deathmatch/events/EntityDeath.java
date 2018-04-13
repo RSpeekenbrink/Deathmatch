a8993499f58088bbd83847a673f803b20d54fb68
Remco Speekenbrink <98remco@gmail.com>
2018-04-13 16:29:27 +0200

    Code Cleanup

--- src/org/rspeekenbrink/deathmatch/events/EntityDeath.java
+++ src/org/rspeekenbrink/deathmatch/events/EntityDeath.java
@@ -8,14 +8,14 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 
 /**
- * EntityDeath Event will handle respawning and all points etc.
+ * EntityDeath Event will handle all points etc.
  * It is registered upon plugin load in the Startup class.
  * 
  * @author 		Remco Speekenbrink
  * @version 	1.0
  * @since       1.0
  */
-public class EntityDeath implements Listener{
+public class EntityDeath implements Listener {
 	
 	@EventHandler
 	public void onPlayerDeath(EntityDeathEvent e) 
