9cb1e69113ff497e019f0c2dd31ac1247ee0b89e
Remco Speekenbrink <98remco@gmail.com>
2018-04-13 17:28:41 +0200

    Added PlayerJoin function

--- src/org/rspeekenbrink/deathmatch/Game.java
+++ src/org/rspeekenbrink/deathmatch/Game.java
@@ -1,9 +1,17 @@
 package org.rspeekenbrink.deathmatch;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.rspeekenbrink.deathmatch.classes.SpawnLocation;
 import org.rspeekenbrink.deathmatch.managers.DatabaseManager;
+import org.rspeekenbrink.deathmatch.managers.MessageManager;
 
 /**
  * Static Class containing all main game functions and loops
@@ -14,17 +22,54 @@ import org.rspeekenbrink.deathmatch.managers.DatabaseManager;
  * @since       1.0
  */
 public final class Game {
-	private DatabaseManager db = DatabaseManager.getInstance();
-	private static ArrayList<Player> inGame = new ArrayList<Player>();
+	private static DatabaseManager db = DatabaseManager.getInstance();
+	private static MessageManager msg = MessageManager.getInstance();
 	
-	public static void PlayerJoin(Player player) {
+	public static ArrayList<Player> inGame = new ArrayList<Player>();
+	
+	/**
+	 * Let a player join the game and spawn
+	 * 
+	 * @param player
+	 * @return boolean Succes
+	 */
+	public static boolean PlayerJoin(Player player) {
+		if(Deathmatch.InMaintenance) {
+			msg.sendErrorMessage("The server is in maintenance!", player);
+			return false;
+		}
+		
+		
+		List<SpawnLocation> gameSpawns = db.getSpawnLocations(SpawnLocation.SpawnType.GAME);
+		if(gameSpawns == null || gameSpawns.isEmpty()) {
+			msg.sendErrorMessage("No spawns found!", player);
+			return false;
+		}
+		
+		Random randomizer = new Random();
+		SpawnLocation randomSpawn = gameSpawns.get(randomizer.nextInt(gameSpawns.size()));
+		
+		player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, (3*20), 1, true, false)); //3 sec blindness if 20ticks/sec
+		player.playSound(randomSpawn, Sound.BLOCK_PORTAL_TRAVEL, 1, 1);
+		player.teleport(randomSpawn);
+		player.setCanPickupItems(true);
+		player.setNoDamageTicks(5 * 20);
+		msg.sendTitleMessage(ChatColor.GREEN + "Good Luck!", "Current Players: " + inGame.size(), player, 20, (6 * 20), 20);
+		
 		inGame.add(player);
+		return true;
 	}
 	
+	/**
+	 * Run All events needed on game start
+	 */
 	public static void Start() {
 		
 	}
 	
+	/**
+	 * Run All events on game stop
+	 */
 	public static void Stop() {
 		
 	}
