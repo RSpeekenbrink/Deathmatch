a645f3a7b0063a8de886d41f3fcecb930856ae4e
Remco Speekenbrink <98remco@gmail.com>
2018-04-14 11:17:56 +0200

    Fixed OP block breaking whilst ingame

--- src/org/rspeekenbrink/deathmatch/events/BlockBreak.java
+++ src/org/rspeekenbrink/deathmatch/events/BlockBreak.java
@@ -4,12 +4,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.rspeekenbrink.deathmatch.Deathmatch;
+import org.rspeekenbrink.deathmatch.Game;
 
 public class BlockBreak implements Listener {
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent e) {
 		if(!Deathmatch.InMaintenance) {
-			if(!(Deathmatch.InDebug && e.getPlayer().isOp())) {
+			if(!(Deathmatch.InDebug && e.getPlayer().isOp()) || Game.inGame.contains(e.getPlayer())) {
 				e.setCancelled(true);
 			}
 		}
--- src/org/rspeekenbrink/deathmatch/events/BlockPlace.java
+++ src/org/rspeekenbrink/deathmatch/events/BlockPlace.java
@@ -4,12 +4,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.rspeekenbrink.deathmatch.Deathmatch;
+import org.rspeekenbrink.deathmatch.Game;
 
 public class BlockPlace implements Listener {
 	@EventHandler
 	public void onBlockPlace(BlockPlaceEvent e) {
 		if(!Deathmatch.InMaintenance) {
-			if(!(Deathmatch.InDebug && e.getPlayer().isOp())) {
+			if(!(Deathmatch.InDebug && e.getPlayer().isOp()) || Game.inGame.contains(e.getPlayer())) {
 				e.setCancelled(true);
 			}
 		}
