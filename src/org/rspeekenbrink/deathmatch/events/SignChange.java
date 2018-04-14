d0547f885863f79e331352103a64985f84c06e2f
Remco Speekenbrink <98remco@gmail.com>
2018-04-14 10:16:15 +0200

    Added creation of join signs

--- src/org/rspeekenbrink/deathmatch/events/SignChange.java
+++ src/org/rspeekenbrink/deathmatch/events/SignChange.java
@@ -1,5 +1,6 @@
 package org.rspeekenbrink.deathmatch.events;
 
+import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
@@ -10,7 +11,14 @@ public class SignChange implements Listener {
 	
 	@EventHandler
 	public void onSignChange(SignChangeEvent e) {
-		
+		if(e.getPlayer().hasPermission("deathmatch.place.sign") || e.getPlayer().isOp()) {
+			if(e.getLine(0).equalsIgnoreCase("[dmjoin]")) {
+				e.setLine(0, "§8[§8Death§8Match§8]");
+				e.setLine(1, "§3Join");
+				e.setLine(2, "§r§fClick here to join");
+				e.setLine(3, "§r§fDeathmatch!");
+			}
+		}
 	}	
 
 }
