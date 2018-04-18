47937a7cf53c9cb76851a0500b381ed2200c3fd2
Remco Speekenbrink <98remco@gmail.com>
2018-04-18 19:16:24 +0200

    Disabled fire spreading

--- src/org/rspeekenbrink/deathmatch/events/BlockSpread.java
+++ src/org/rspeekenbrink/deathmatch/events/BlockSpread.java
@@ -1,5 +1,6 @@
 package org.rspeekenbrink.deathmatch.events;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockSpreadEvent;
@@ -7,6 +8,8 @@ import org.bukkit.event.block.BlockSpreadEvent;
 public class BlockSpread implements Listener {
 	@EventHandler
 	public void onBlockSpread(BlockSpreadEvent e) {
-		
+		if(e.getSource().getType() == Material.FIRE) {
+			e.setCancelled(true);
+		}
 	}
 }
